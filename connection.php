9712002cf060a789bb2a4ffac7fae6d13de9103e
RiZhii <143491670+RiZhii@users.noreply.github.com>
2025-08-11 22:20:48 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = mysqli_connect('localhost','root','','musical_world');
+	$conn = mysqli_connect('localhost','root','admin','musical_world');
 
 	if(!$conn)
 		die("Error while connecting...!").mysqli_connect_error($conn);
